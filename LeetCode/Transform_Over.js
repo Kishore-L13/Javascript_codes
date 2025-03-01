9d03060614c074a31fe947062bf1b64088476a29
kishore <kishore.krrish13@gmail.com>
2025-03-01 22:03:49 +0530

    Transform_Over.js

---
+++ LeetCode/Transform_Over.js
@@ -0,0 +1,14 @@
+var map = function(arr, fn) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+    return result;
+};
+
+// Example usage:
+function plusone(n) {
+    return n + 1;
+}
+
+console.log(map([1, 2, 3], plusone)); // Output: [2,3,4]
