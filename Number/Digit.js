38f0500d43bd8a27c51e4160aa90e31bc50268c2
kishore <kishore.krrish13@gmail.com>
2025-04-15 22:27:15 +0530

    Digit

---
+++ Number/Digit.js
@@ -0,0 +1,14 @@
+const countDigits = x => {
+    let count = 0;
+    while (x > 0) {
+      x = Math.floor(x / 10);
+      count++;
+    }
+    return count;
+  };
+  console.log(countDigits(80));
+  
+  const countDigits1 = x => {
+    return Math.floor(Math.log10(x) + 1);
+  };
+  console.log(countDigits1(801234));
