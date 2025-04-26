834604d669a4cce36206bce92c65c24f7e493fe2
kishore <kishore.krrish13@gmail.com>
2025-04-26 22:41:57 +0530

    GroupBy

---
+++ Namaste_INV/GroupBy.js
@@ -0,0 +1,19 @@
+function groupBy(arr, key) {
+    // Your implementation
+  const result = {};
+  for (const item of arr){
+    const keyValue = item[key];
+    if (!result.hasOwnProperty(keyValue)){
+      result[keyValue] = []
+    }
+    result[keyValue].push(item)
+  }
+  return result
+}
+const a = [
+    { name: "Abc", age: "25" },
+    { name: "efg", age: "25" },
+    { name: "xyz", age: "30" }
+  ];
+  
+  console.log(groupBy(a, 'age'));
