950690b8f14cb1efd1c0ffbff90d1c9eb8989da2
kishore <kishore.krrish13@gmail.com>
2025-04-27 21:31:15 +0530

    chunkyarray

---
+++ Namaste_INV/chunkyarray.js
@@ -0,0 +1,9 @@
+function chunkArray(arr, n) {
+    // Your implementation
+  const result = [];
+  for (let i=0; i<arr.length;i+=n){
+    result.push(arr.slice(i,i+n))
+  }
+  return result;
+}
+console.log(chunkArray([1,2,3,4],2))
