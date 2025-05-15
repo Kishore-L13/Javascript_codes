c36ff96f2e386da321bf60ecc7b2b6e6b2630079
kishore <kishore.krrish13@gmail.com>
2025-05-15 22:49:09 +0530

    Vowel

---
+++ Namaste_INV/Vowel.js
@@ -0,0 +1,13 @@
+const Vowel = (str) => {
+    let count = 0;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    const k = str.toLowerCase();
+    for (let c of k) {
+        if (vowels.includes(c)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(Vowel("abca")); // Output: 2
