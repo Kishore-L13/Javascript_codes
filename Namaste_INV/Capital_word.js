513b6a05f43ce80ba31e95270bc74c248d6a6985
kishore <kishore.krrish13@gmail.com>
2025-05-20 22:26:10 +0530

    Capital_word

--- Namaste_INV/Capital_word.js
+++ Namaste_INV/Capital_word.js
@@ -1,12 +1,18 @@
+// const Capital_word = (str) => {
+// let result = ""
+//     for (let i=0;i<str.length;i++){
+//         if (str[i-1]===""||i===0){
+//             result=str[i].toUpperCase()
+//         }else{
+//             result+=str[i]
+//         }
+//     }
+//     return result
+// }
 const Capital_word = (str) => {
-let result = ""
-    for (let i=0;i<str.length;i++){
-        if (str[i-1]===""||i===0){
-            result=str[i].toUpperCase()
-        }else{
-            result+=str[i]
-        }
-    }
-    return result
+  // Your implementation
+    return str.trim()
+        .split(/\s+/)
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
 }
 console.log(Capital_word("hello world"))
