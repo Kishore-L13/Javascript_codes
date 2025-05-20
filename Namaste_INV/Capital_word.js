4b1384ad45982b21183a8b1e81ec043db507cacd
kishore <kishore.krrish13@gmail.com>
2025-05-20 22:23:15 +0530

    Capital_word

---
+++ Namaste_INV/Capital_word.js
@@ -0,0 +1,12 @@
+const Capital_word = (str) => {
+let result = ""
+    for (let i=0;i<str.length;i++){
+        if (str[i-1]===""||i===0){
+            result=str[i].toUpperCase()
+        }else{
+            result+=str[i]
+        }
+    }
+    return result
+}
+console.log(Capital_word("hello world"))
