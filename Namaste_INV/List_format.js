9b4f4de6a7574a369ed8c34645f37b8da62c45a0
kishore <kishore.krrish13@gmail.com>
2025-05-31 22:44:09 +0530

    List_Format

---
+++ Namaste_INV/List_format.js
@@ -0,0 +1,13 @@
+function formatList(items) {
+  // your implementation
+  const len = items.length;
+  if (len === 0) return "";
+  if (len === 1) return items[0];
+  if (len === 2) return `${items[0]} and ${items[1]}`
+
+  return `${items.slice(0,len-1).join(", ")} and ${items[len-1]}`
+}
+
+//For the purpose of user debugging.
+console.log(formatList(["apple", "banana"]));
+
