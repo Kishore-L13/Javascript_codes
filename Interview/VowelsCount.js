a3997bfb8bd9e00acc2c90538c41ab5d9add13ca
kishore <kishore.krrish13@gmail.com>
2025-06-01 22:45:38 +0530

    VCount

---
+++ Interview/VowelsCount.js
@@ -0,0 +1,12 @@
+const V = (str) => {
+    let vowels = ['a','e','i','o','u']
+    let count = 0
+    let VCount = {}
+        for (let i=0;i<str.length;i++){
+        if(vowels.includes(str[i])){
+VCount[char] = (VCount[str[i]] || 0) + 1;
+            count ++
+        }
+    }
+    return (count,VCount) 
+}
