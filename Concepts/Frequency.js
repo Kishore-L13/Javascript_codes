6fd288f898dddfbec34f5ac50d712773b05f114f
kishore <kishore.krrish13@gmail.com>
2025-06-23 17:32:26 +0530

    Frequency

---
+++ Concepts/Frequency.js
@@ -0,0 +1,27 @@
+const sentences = [
+  "JavaScript is great, and JavaScript is fun!",
+  "I love coding in JavaScript.",
+  "Coding is fun when JavaScript works."
+];
+
+const MostFreqTop3 = (str) => {
+  const words = str
+    .join(" ")
+    .replace(/[^\w\s]/g, "") // remove punctuation
+    .toLowerCase()
+    .split(/\s+/);
+
+  const freqMap = {};
+  for (let word of words) {
+    freqMap[word] = (freqMap[word] || 0) + 1;
+  }
+
+  // Sort words by frequency descending
+  const sorted = Object.entries(freqMap)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 4) // top 3
+
+  return sorted.map(([word]) => word);
+};
+
+console.log(MostFreqTop3(sentences)); // ['javascript', 'is', 'coding','Fun']
