0920e087e0364c240ee04e8f46834e77f6d99eba
kishore <kishore.krrish13@gmail.com>
2025-06-24 11:48:53 +0530

    Frequency

--- Concepts/Frequency_an.js
+++ Concepts/Frequency_an.js
@@ -19,3 +19,25 @@ const getFrequentWords = (sentences) => {
 };
 
 console.log(getFrequentWords(sentences)); // Output: javascript is coding
+
+// // Online Javascript Editor for free
+// // Write, Edit and Run your Javascript code using JS Online Compiler
+// const sentences = [
+//   "JavaScript is great, and JavaScript is fun.",
+//   "I love coding in JavaScript.",
+//   "Coding is fun when JavaScript works."
+// ];
+
+// const max = (str) => {
+//     const text = sentences.join(" ").toLowerCase().replace(/[.,]/g, "");
+//   const words = text.split(/\s+/);
+//     let freq = {}
+//     for (let word of words){
+//         freq[word] = (freq[word]||0) + 1 
+//         }
+//             //console.log(freq)
+//      const result = Object.keys(freq).filter(word => freq[word] >= 2);
+
+//   return result.join(" ");
+// }
+// console.log(max(sentences))
